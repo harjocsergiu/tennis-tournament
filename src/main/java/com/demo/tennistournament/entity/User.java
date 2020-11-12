2cbe0a4c162d69382a35c889aa86c11af44bf504
SHarjoc <sergiuioan.harjoc@microfocus.com>
2020-11-12 21:15:29 +0200

    Add User entity

---
+++ src/main/java/com/demo/tennistournament/entity/User.java
@@ -0,0 +1,51 @@
+package com.demo.tennistournament.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private boolean admin;
+    //private String profilePicture
+
+    protected User(){}
+
+    public User(String email, String password, String firstName, String lastName) {
+        super();
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.admin = false;
+    }
+
+    public User(String email, String password, String firstName, String lastName, boolean admin) {
+        super();
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.admin = admin;
+    }
+}
